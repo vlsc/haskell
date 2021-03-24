f1f85d1d89e2eacfaf061c1a9d03e62b226677b3
Victória <56272572+vlsc@users.noreply.github.com>
2021-03-23 22:32:00 -0300

    Update editText.hs

--- editText.hs
+++ editText.hs
@@ -4,11 +4,9 @@ data Cmd = Cursor Int |
            Insert String
            deriving (Read)
  
-
 separaStringE s 0 = []
 separaStringE s n = (head s):separaStringE (tail s) (n-1)
 
---junta :: [String] -> Int -> [String]
 junta s 0 = ([], s)
 junta s n = (separaStringE s n, separaStringD s n)
 
@@ -16,20 +14,14 @@ separaStringD [] 0 = []
 separaStringD s 0 = (head s):separaStringD (tail s) 0
 separaStringD s n = separaStringD (tail s) (n-1)
 
-
---apaga x caracteres atas do cursor, não incluindo o apontado por ele
 back s 0 = []
 back s n = (head s):back (tail s) (n-1)
-
-
 
 del [] 0 = []
 del s 0 = (head s):del (tail s) 0
 del s n = del (tail s) (n-1)
 
-
 insert s (Insert f) curPos = fst(junta s curPos)++f++snd(junta s curPos)
-
 
 comandos :: String -> [Cmd] -> Int -> String
 comandos s [] curPos = s
@@ -38,13 +30,9 @@ comandos s a curPos | fst(checa (head a)) == 1 = comandos s (tail a) (curPos+snd
                     | fst(checa (head a)) == 3 = comandos ((fst(junta s (curPos)))++(del(snd(junta s (curPos))) (snd(checa (head a))))) (tail a) (curPos) 
                     | fst(checa (head a)) == 4 = comandos (insert s (head a) curPos) (tail a) curPos    
 
-
 checa (Cursor n) = (1, n)
 checa (Backspace n) = (2, n)
 checa (Delete n) = (3, n)
 checa (Insert f) = (4,0)
 
 editText s a = comandos s a 0
-
-
-
