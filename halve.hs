8e4b3053f43f735bee466481ea8f627512f1a449
Victória <56272572+vlsc@users.noreply.github.com>
2021-03-23 22:33:39 -0300

    Update halve.hs

--- halve.hs
+++ halve.hs
@@ -1,10 +1,7 @@
-
 separa :: Eq t =>[t] -> ([t],[t])
 separa l | l == [] = ([],[])
          | otherwise = (listaImpar l,listaPar l)
-         
 
---listaImpar :: Eq a => [a] -> [a]
 listaImpar :: Eq a => [a] -> [a]
 listaImpar l | l == [] = []
              | tail l == [] = head l : []
@@ -13,8 +10,3 @@ listaImpar l | l == [] = []
 listaPar l | l == [] = []
            | tail l == [] = []
            | otherwise = head (tail l) : listaPar(tail (tail l))
-                
-
-
-
-
