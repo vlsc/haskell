573ee5ddc97f4179e07c46e0a60c95fdf4ec1e4e
Victória <56272572+vlsc@users.noreply.github.com>
2021-03-23 22:28:24 -0300

    Update isBST.hs

--- isBST.hs
+++ isBST.hs
@@ -1,9 +1,6 @@
-
 data Tree t = Nilt |
               Node t (Tree t) (Tree t)
               deriving (Read)
-              
-  
 
 verif1 n0 (Node n t1 t2) | n0 > n = verif1 n0 t1 && verif1 n0 t2
                          | otherwise = False
@@ -13,10 +10,7 @@ verif2 n0 (Node n t1 t2) | n0 < n = verif2 n0 t1 && verif2 n0 t2
                          | otherwise = False
 verif2 n0 (Nilt) = True
 
-
 isBST :: Ord t => Tree t -> Bool
 isBST (Nilt) = True
 isBST (Node n t1 t2) | verif1 n t1 && verif2 n t2 = isBST t1 && isBST t2
                      | otherwise = False
-
-
