0fe8b8c509258331241af2cace63af4ab1259b0c
Victória <56272572+vlsc@users.noreply.github.com>
2021-03-23 22:32:18 -0300

    Update evalTree.hs

--- evalTree.hs
+++ evalTree.hs
@@ -1,11 +1,9 @@
-
 data Ops = SUM | MUL | SUB
            deriving (Read)
 
 data IntTree = Nilt Int |
                Node Ops IntTree IntTree
                deriving (Read)
-
 
 evalTree :: IntTree -> Int
 evalTree (Nilt n) = n
